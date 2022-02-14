3c89e775a2766c01e0bb9cf3f40bbc3994df22fa
iyb-wangyue <wangyue@iyunbao.com>
2022-02-14 10:28:02 +0800

    node start

---
+++ 01base/02-buffer.js
@@ -0,0 +1,6 @@
+// let buf = Buffer.alloc(10)
+// console.log(buf);
+
+let buf = Buffer.from('hello world')
+console.log(buf);
+console.log(buf.toString());
---
+++ 01base/03-http.js
@@ -0,0 +1,10 @@
+
+
+const http = require("http");
+
+const fs = require("fs");
+
+
+let server = http.createServer((req, res) => {
+
+})
