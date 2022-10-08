6b812a48c87492d5c2f7c31ab33188eb592c7713
iyb-wangyue <wangyue@iyunbao.com>
2022-10-08 18:22:39 +0800

    feat: express练习

--- node/01/api/03-fs.js
+++ node/01/api/03-fs.js
@@ -1,10 +1,12 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path')
 
 // fs.readFile('./download.js', (error, data) => {
 //   if(error) throw error;
   
 //   console.log(data);
 // })
+console.log(__dirname)
 
-const data = fs.readFileSync('./01-run.js')
+const data = fs.readFileSync(__dirname + '/01-run.js')
 console.log(data);
--- node/02/express/index.js
+++ node/02/express/index.js
@@ -1,25 +1,29 @@
 const express = require('express')
-const app = express()
-app.listen(3000)
 
-const mid1 = (req, res, next) => {
-    console.log(1)
+const app = express()
+
+app.listen(3000, () => {
+    console.log('server is at port: 3000')
+})
+
+const mid1 = (req,res, next) => {
+    console.log('1')
     res.write('a')
     next()
-    console.log(2)
+    console.log('2')
 }
 
 const mid2 = (req, res, next) => {
-    console.log(3)
-    res.write('b')
+    console.log('3')
     next()
+    res.write('b')
     res.write('d')
     res.end()
     console.log(4)
 }
 
 const mid3 = (req, res, next) => {
-    console.log(5)
+    console.log('5')
     next()
     res.write('c')
     console.log(6)
